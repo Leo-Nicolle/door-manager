59ea2565b4b0845124483f6413c2d1d75b8d5ce7
Leo Nicolle <leo@linkurio.us>
2020-08-18 22:41:37 +0200

    abstract groups

--- client/src/components/Groups.vue
+++ client/src/components/Groups.vue
@@ -1,45 +1,51 @@
 <template>
   <div class="groups">
-    <table>
-      <thead>
-        <tr>
-          <th>Nom</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(group, i) in groups" :key="i" @click="onGroupClick(group)">
-          <td>{{ group.name }}</td>
-        </tr>
-      </tbody>
-    </table>
-    <button @click="onAddGroup()">Ajouter Group</button>
-    <Modal v-if="selectedGroup">
-      <Group :group="selectedGroup" :doors="doors" @cancel="onCancel()" />
-    </Modal>
+    <ElementsDisplay
+      :elements="elements"
+      :selectedElement="selectedElement"
+      @queryResult="onQueryResult"
+      @add="onAddElement"
+    >
+      <tr slot="headers">
+        <th>Nom</th>
+      </tr>
+      <tr
+        slot="body"
+        v-for="(group, i) in filteredElements"
+        :key="i"
+        @click="onElementClick(group)"
+      >
+        <td>{{ group.name }}</td>
+      </tr>
+      <Group
+        slot="form"
+        :doors="doors"
+        :group="selectedElement"
+        @cancel="onCancel()"
+        @submit="onSubmit()"
+      />
+    </ElementsDisplay>
   </div>
 </template>
 
 <script>
 import Group from "./Group";
-import Modal from "./Modal";
 import { getUrl } from "../js/utils";
 import axios from "axios";
+import ElementsDisplay from "../mixins/ElementsDisplay";
+import ElementsDisplayMixin from "../mixins/ElementsDisplayMixin.js";
 
 export default {
   name: "Groups",
   data() {
     return {
-      selectedGroup: null,
       doors: [],
-      groups: [],
     };
   },
+  mixins: [ElementsDisplayMixin],
   methods: {
-    onGroupClick(group) {
-      this.selectedGroup = group;
-    },
-    onAddGroup() {
-      this.selectedGroup = {
+    onAddElement() {
+      this.selectedElement = {
         name: "",
         doorAccess: this.doors.reduce((doorAcces, { id }) => {
           doorAcces[id] = "";
@@ -47,24 +53,14 @@ export default {
         }, {}),
       };
     },
-    fetchDoors() {
+    fetch() {
       axios.get(getUrl("door")).then(({ data }) => (this.doors = data));
+      axios.get(getUrl("group")).then(({ data }) => (this.elements = data));
     },
-    fetchGroups() {
-      axios.get(getUrl("group")).then(({ data }) => (this.groups = data));
-    },
-
-    onCancel() {
-      this.selectedGroup = null;
-    },
-  },
-  mounted() {
-    this.fetchDoors();
-    this.fetchGroups();
   },
   components: {
     Group,
-    Modal,
+    ElementsDisplay,
   },
 };
 </script>
