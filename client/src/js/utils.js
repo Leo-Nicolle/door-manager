473fd4fdd5eb0955761d9cf14282dbecbbd99070
Leo Nicolle <leo@linkurio.us>
2020-08-23 17:01:54 +0200

    fix paths

--- client/src/js/utils.js
+++
@@ -1,5 +0,0 @@
-export function getUrl(route) {
-  const url = `http://${process.env.VUE_APP_SERVER_IP}:${process.env.VUE_APP_SERVER_PORT}/${route}`;
-  console.log(url);
-  return url;
-}
--- scripts/configure.js
+++
@@ -1,8 +0,0 @@
-import { fstat } from "fs";
-import encrypt from "quick-encrypt";
-
-function generateAndWriteKeys() {
-  const keys = encrypt.generate(1024);
-
-  fs.writeFile("db/keys.json", JSON.stringify(keys), {}, () => {});
-}
